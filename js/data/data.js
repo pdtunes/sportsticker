1e412cec5dbe96ef5181421b0a2e4ec864b07263
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-09-30 23:37:06 +0200

    update

--- js/data/data.js
+++ js/data/data.js
@@ -1,20 +1,5 @@
 export const NFL_API =
- /*  "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"; */
+  "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard";
 
-(async function () {
 
-const scoreContainer = document.querySelector(".container")
-  const scoreURL = `${NFL_API}`;
 
-  try {
-    const response = await fetch(scoreURL);
-    const scores = await response.json();
-
-    scoreContainer.innerHTML = "";
-    scores.forEach(score => {
-      console.log(score);
-    });
-  } catch (error) {
-    error = ("Nothing here", error);
-  }
-})();
--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 import { NFL_API } from "./data/data.js";
 
+(async function () {
+    const scoreContainer = document.querySelector(".container");
+    const scoreURL = `${NFL_API}`;
+  
+    try {
+      const response = await fetch(scoreURL);
+      const nfl = await response.json();
+  
+      scoreContainer.innerHTML = "";
+      nfl.leagues.forEach(score => {
+          console.log(nfl);
+        nfl.events.forEach(event => {
+        console.log(event.shortName);
+        scoreContainer.innerHTML = `
+        <h4>${score.abbreviation}</h4> 
+        <p>${event.shortName}<p>
+        `;
+        
+        })  
+    });
+
+    } catch (error) {
+      error = ("Nothing here", error);
+      console.log(error);
+    }
+  })();
+  
