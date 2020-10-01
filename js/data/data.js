9da293947c9f43aa8c2d873a12b7cd02e257cc90
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-10-01 21:14:14 +0200

    update

--- js/data/data.js
+++ js/data/data.js
@@ -2,4 +2,6 @@ export const NFL_API =
   "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard";
 
 
+  export const MLB_API = "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard";
+
 
--- js/script.js
+++ js/script.js
@@ -1,34 +1,72 @@
 import { NFL_API } from "./data/data.js";
+import { MLB_API } from "./data/data.js";
+
 
 (async function () {
-  const nflContainer = document.querySelector(".card-deck");
-  const scoreURL = `${NFL_API}`;
+  const nflContainer = document.querySelector(".nfl");
+  const nflURL = `${NFL_API}`;
 
   try {
-    const response = await fetch(scoreURL);
-    const nfl = await response.json();
+    const nflResponse = await fetch(nflURL);
+    const nfl = await nflResponse.json();
 
-    nflContainer.innerHTML = "";
+    nflContainer.innerHTML = " <h1>NFL</h1>";
 
-    nfl.events.forEach(event => {
-      event.competitions.forEach(competition => {
+    nfl.events.forEach(nflEvent => {
+      nflEvent.competitions.forEach(nflCompetition => {
         console.log(nfl);
 
-        const [homeTeam, awayTeam] = competition.competitors;
+        const [nflHomeTeam, nflAwayTeam] = nflCompetition.competitors;
         nflContainer.innerHTML += `
+       
         <div class="card-deck">
             <div class="card">
                 <div class="card-body">
-                <h5 class="card-title"> ${awayTeam.team.location} ${awayTeam.team.name} <img class="logo" src=" ${awayTeam.team.logo}"> @ ${homeTeam.team.location} ${homeTeam.team.name} <img class="logo" src=" ${homeTeam.team.logo}">  </h5>
+                <h5 class="card-title"> ${nflAwayTeam.team.location} ${nflAwayTeam.team.name} <img class="logo" src=" ${nflAwayTeam.team.logo}"> @ ${nflHomeTeam.team.location} ${nflHomeTeam.team.name} <img class="logo" src=" ${nflHomeTeam.team.logo}">  </h5>
                 <p class="card-text">Score: </p>
-                <p class="card-text">Time: ${event.status.displayClock} Period ${event.status.period}</p>
+                <p class="card-text">Time: ${nflEvent.status.displayClock} Period: ${nflEvent.status.period}</p>
                 </div>
             </div>
             </div>
       `;
       });
     });
+  } catch (error) {
+    error = ("Nothing here", error);
+    console.log(error);
+  }
+})();
 
+(async function () {
+  const mlbContainer = document.querySelector(".mlb");
+  const mlbURL = `${MLB_API}`;
+  
+
+  try {
+    const mlbResponse = await fetch(mlbURL);
+    const mlb = await mlbResponse.json();
+
+
+    mlbContainer.innerHTML = " <h1>MLB</h1>";
+
+    mlb.events.forEach(mlbEvent => {
+      mlbEvent.competitions.forEach(mlbCompetition => {
+        console.log(mlb);
+
+        const [mlbHomeTeam, mlbAwayTeam] = mlbCompetition.competitors;
+        mlbContainer.innerHTML += `
+        <div class="card-deck">
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title"> ${mlbAwayTeam.team.location} ${mlbAwayTeam.team.name} <img class="logo" src=" ${mlbAwayTeam.team.logo}"> @ ${mlbHomeTeam.team.location} ${mlbHomeTeam.team.name} <img class="logo" src=" ${mlbHomeTeam.team.logo}">  </h5>
+                <p class="card-text">Score: </p>
+                <p class="card-text"> Inning: ${mlbEvent.status.period}</p>
+                </div>
+            </div>
+            </div>
+      `;
+      });
+    });
   } catch (error) {
     error = ("Nothing here", error);
     console.log(error);
