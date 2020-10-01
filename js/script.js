2dfaba64066251a8cf83cc480886cdaabb4d1c42
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-10-01 10:01:00 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,32 +1,42 @@
 import { NFL_API } from "./data/data.js";
 
 (async function () {
-    const scoreContainer = document.querySelector(".container");
-    const scoreURL = `${NFL_API}`;
-  
-    try {
-      const response = await fetch(scoreURL);
-      const nfl = await response.json();
-  
-      scoreContainer.innerHTML = "";
-      nfl.leagues.forEach(score => {
-        scoreContainer.innerHTML = `
+  const scoreContainer = document.querySelector(".container");
+  const scoreURL = `${NFL_API}`;
+
+  try {
+    const response = await fetch(scoreURL);
+    const nfl = await response.json();
+
+    scoreContainer.innerHTML = "";
+    nfl.leagues.forEach(score => {
+      scoreContainer.innerHTML = `
         <h4>${score.abbreviation}</h4> 
         `;
 
-     
-        nfl.events.forEach(event => {
-        console.log(event);
-        scoreContainer.innerHTML += `
-        <th>${event.shortName}<th>
-      `
-        
-        })  
-    });
+   
 
-    } catch (error) {
-      error = ("Nothing here", error);
-      console.log(error);
-    }
-  })();
-  
+      nfl.events.forEach(event => {
+        event.competitions.forEach(competition => {
+            competition.competitors.forEach(competitor => {
+                console.log(competition.competitors);
+        scoreContainer.innerHTML += `
+<div class="card-group">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">${event.shortName}</h5>
+      <p class="card-text">score</p>
+      <p class="card-text">${event.status.displayClock}</p>
+    </div>
+  </div>
+</div>
+      `;
+        })
+      });
+    })
+    });
+  } catch (error) {
+    error = ("Nothing here", error);
+    console.log(error);
+  }
+})();
