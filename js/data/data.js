dda226e9b46e4653aad28f7b7f5e9f8a611c28ac
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-10-13 23:52:08 +0200

    update

--- js/data/data.js
+++ js/data/data.js
@@ -1,10 +1,11 @@
-export const NFL_API =
-  "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard";
+/* export const NFL_API =
+  "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"; */
 
-  export const NFL_SCORE = "http://static.nfl.com/liveupdate/scores/scores.json";
+export const NFL_SCORE = "http://static.nfl.com/liveupdate/scores/scores.json";
+
+/* export const MLB_API =
+  "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard"; */
 
 
-
-  export const MLB_API = "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard";
-
-
+export const NHL_SCORE =
+  "http://statsapi.web.nhl.com/api/v1/game/2015020742/feed/live";
--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 import { NFL_SCORE } from "./data/data.js";
+import { NHL_SCORE } from "./data/data.js";
 
 /* import { MLB_API } from "./data/data.js"; */
 
 //NFL
-
 (async function () {
   const nflContainer = document.querySelector(".nfl");
   const scoreURL = `${NFL_SCORE}`;
@@ -12,33 +12,29 @@ import { NFL_SCORE } from "./data/data.js";
     const nflScoreResponse = await fetch(scoreURL);
     const nflScore = await nflScoreResponse.json();
 
-    nflContainer.innerHTML = "<h1>NFL</h1>";
+    /* nflContainer.innerHTML = "<h1>NFL</h1>"; */
 
     for (let item in nflScore) {
-      console.log(nflScore);
       const hometeamName = nflScore[item].home.abbr;
       const awayteamName = nflScore[item].away.abbr;
-      const hometeam = nflScore[item].home.score.T;
-      const awayteam = nflScore[item].away.score.T;
+      const hometeamScore = nflScore[item].home.score.T;
+      const awayteamScore = nflScore[item].away.score.T;
       const stadium = nflScore[item].stadium;
       const quarter = nflScore[item].qtr;
       const clock = nflScore[item].clock;
 
       nflContainer.innerHTML += `
-      <div class=".card-deck">
-            <div class="card">
+
+      <div class="card">
                 <div class="card-body">
                 <h5 class="card-title"><img class="homelogo" src="./assets/nfl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nfl/${awayteamName}.png" </img> </h5>
-                <p class="card-text">Score: ${hometeam} - ${awayteam}</p>
+                <p class="card-text">Score: ${hometeamScore} - ${awayteamScore}</p>
                 <p class="card-text">Stadium: ${stadium} </p>
                 <p class="card-text">Quarter: ${quarter} </p>
                 <p class="card-text">Clock: ${clock} </p>
-               
-                </div>
-                </div>
             </div>
             </div>
-       
+            </div>
       `;
     }
   } catch (error) {
@@ -47,35 +43,42 @@ import { NFL_SCORE } from "./data/data.js";
   }
 })();
 
-/* //MLB
-
+//NHL
 (async function () {
-  const mlbContainer = document.querySelector(".mlb");
-  const mlbURL = `${MLB_API}`;
+  const nhlContainer = document.querySelector(".nhl");
+  const scoreURL = `${NHL_SCORE}`;
 
   try {
-    const mlbResponse = await fetch(mlbURL);
-    const mlb = await mlbResponse.json();
+    const nhlScoreResponse = await fetch(scoreURL);
+    const nhlScore = await nhlScoreResponse.json();
+    console.log(nhlScore);
 
-    mlbContainer.innerHTML = " <h1>MLB</h1>";
+    for (let item in nhlScore) {
+      const hometeamName = nhlScore.gameData.teams.home.abbreviation;
+      console.log(nhlScore);
+      const awayteamName = nhlScore.gameData.teams.away.abbreviation;
+         /*    const hometeamScore = nflScore[item].home.score.T;
+      const awayteamScore = nflScore[item].away.score.T;
+      const stadium = nflScore[item].stadium;
+      const quarter = nflScore[item].qtr;
+      const clock = nflScore[item].clock; */
 
-    mlb.events.forEach(mlbEvent => {
-      mlbEvent.competitions.forEach(mlbCompetition => {
-        const [mlbHomeTeam, mlbAwayTeam] = mlbCompetition.competitors;
-        mlbContainer.innerHTML += `
-        <div class="card-deck">
-            <div class="card">
+      /*   nhlContainer.innerHTML += `
+
+      <div class="card">
                 <div class="card-body">
-                <h5 class="card-title"> ${mlbAwayTeam.team.location} ${mlbAwayTeam.team.name} <img class="logo" src=" ${mlbAwayTeam.team.logo}"> @ ${mlbHomeTeam.team.location} ${mlbHomeTeam.team.name} <img class="logo" src=" ${mlbHomeTeam.team.logo}">  </h5>
-                <p class="card-text"> Inning: ${mlbEvent.status.period} || Game status: ${mlbEvent.status.type.description}</p>
-                </div>
+                <h5 class="card-title"><img class="homelogo" src="./assets/nfl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nfl/${awayteamName}.png" </img> </h5>
+                <p class="card-text">Score: ${hometeamScore} - ${awayteamScore}</p>
+                <p class="card-text">Stadium: ${stadium} </p>
+                <p class="card-text">Quarter: ${quarter} </p>
+                <p class="card-text">Clock: ${clock} </p>
             </div>
             </div>
-      `;
-      });
-    });
+            </div>
+      `; */
+    }
   } catch (error) {
     error = ("Nothing here", error);
     console.log(error);
   }
-})(); */
+})();
