aac9b963b63f523852d49855965eb1f0152ee335
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-10-14 11:25:58 +0200

    update

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ import { NHL_SCORE } from "./data/data.js";
 })();
 
 //NHL
-(async function () {
+/* (async function () {
   const nhlContainer = document.querySelector(".nhl");
   const scoreURL = `${NHL_SCORE}`;
 
@@ -52,22 +52,28 @@ import { NHL_SCORE } from "./data/data.js";
     const nhlScoreResponse = await fetch(scoreURL);
     const nhlScore = await nhlScoreResponse.json();
     console.log(nhlScore);
-
-    for (let item in nhlScore) {
+    
+ if number of objects is 1
+  output 1 object
+else
+  loop through all objects 
+    
       const hometeamName = nhlScore.gameData.teams.home.abbreviation;
-      console.log(nhlScore.gameData.venue.name);
       const awayteamName = nhlScore.gameData.teams.away.abbreviation;
-       const hometeamScore = nhlScore.liveData.plays.currentPlay.about.goals.home
-      const awayteamScore = nhlScore.liveData.plays.currentPlay.about.goals.away
-           const stadium = nhlScore.gameData.venue.name;
+      const hometeamScore =
+        nhlScore.liveData.plays.currentPlay.about.goals.home;
+      const awayteamScore =
+        nhlScore.liveData.plays.currentPlay.about.goals.away;
+      const stadium = nhlScore.gameData.venue.name;
       const period = nhlScore.liveData.plays.currentPlay.period;
       const clock = nhlScore.liveData.plays.currentPlay.periodTimeRemaining;
-
-        nhlContainer.innerHTML += `
+      console.log(hometeamName, awayteamName);
+      nhlContainer.innerHTML += `
+      
 
       <div class="card">
                 <div class="card-body">
-                <h5 class="card-title"><img class="homelogo" src="./assets/nfl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nfl/${awayteamName}.png" </img> </h5>
+                <h5 class="card-title"><img class="homelogo" src="./assets/nhl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nhl/${awayteamName}.png" </img> </h5>
                 <p class="card-text">Score: ${hometeamScore} - ${awayteamScore}</p>
                 <p class="card-text">Stadium: ${stadium} </p>
                 <p class="card-text">Period: ${period} </p>
@@ -76,9 +82,10 @@ import { NHL_SCORE } from "./data/data.js";
             </div>
             </div>
       `;
-    }
+    
   } catch (error) {
     error = ("Nothing here", error);
     console.log(error);
   }
 })();
+ */
