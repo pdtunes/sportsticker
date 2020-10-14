018a47080c7e23217e6c04f3234bd84f079b69ba
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-10-14 10:01:44 +0200

    update

--- js/script.js
+++ js/script.js
@@ -55,27 +55,27 @@ import { NHL_SCORE } from "./data/data.js";
 
     for (let item in nhlScore) {
       const hometeamName = nhlScore.gameData.teams.home.abbreviation;
-      console.log(nhlScore);
+      console.log(nhlScore.gameData.venue.name);
       const awayteamName = nhlScore.gameData.teams.away.abbreviation;
-         /*    const hometeamScore = nflScore[item].home.score.T;
-      const awayteamScore = nflScore[item].away.score.T;
-      const stadium = nflScore[item].stadium;
-      const quarter = nflScore[item].qtr;
-      const clock = nflScore[item].clock; */
+       const hometeamScore = nhlScore.liveData.plays.currentPlay.about.goals.home
+      const awayteamScore = nhlScore.liveData.plays.currentPlay.about.goals.away
+           const stadium = nhlScore.gameData.venue.name;
+      const period = nhlScore.liveData.plays.currentPlay.period;
+      const clock = nhlScore.liveData.plays.currentPlay.periodTimeRemaining;
 
-      /*   nhlContainer.innerHTML += `
+        nhlContainer.innerHTML += `
 
       <div class="card">
                 <div class="card-body">
                 <h5 class="card-title"><img class="homelogo" src="./assets/nfl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nfl/${awayteamName}.png" </img> </h5>
                 <p class="card-text">Score: ${hometeamScore} - ${awayteamScore}</p>
                 <p class="card-text">Stadium: ${stadium} </p>
-                <p class="card-text">Quarter: ${quarter} </p>
+                <p class="card-text">Period: ${period} </p>
                 <p class="card-text">Clock: ${clock} </p>
             </div>
             </div>
             </div>
-      `; */
+      `;
     }
   } catch (error) {
     error = ("Nothing here", error);
