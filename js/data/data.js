f3b516d797dc783753937f3761542be2e0b029f8
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-10-20 13:46:19 +0200

    update

--- js/data/data.js
+++ js/data/data.js
@@ -1,11 +1,6 @@
-/* export const NFL_API =
-  "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"; */
+
 
 export const NFL_SCORE = "http://static.nfl.com/liveupdate/scores/scores.json";
 
-/* export const MLB_API =
-  "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard"; */
-
-
 export const NHL_SCORE =
-  "http://statsapi.web.nhl.com/api/v1/game/2015020742/feed/live";
+  "https://nhl-score-api.herokuapp.com/api/scores/latest";
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import { NFL_SCORE } from "./data/data.js";
-
+import { NHL_SCORE } from "./data/data.js";
 
 //NFL
 (async function () {
@@ -9,9 +9,6 @@ import { NFL_SCORE } from "./data/data.js";
   try {
     const nflScoreResponse = await fetch(scoreURL);
     const nflScore = await nflScoreResponse.json();
-
-    console.log(nflScore);
-
 
     for (let item in nflScore) {
       const hometeamName = nflScore[item].home.abbr;
@@ -43,48 +40,45 @@ import { NFL_SCORE } from "./data/data.js";
 })();
 
 //NHL
-/* (async function () {
-  const nhlContainer = document.querySelector(".nhl");
+
+(async function () {
+  const nhlContainer = document.querySelector(".nfl");
   const scoreURL = `${NHL_SCORE}`;
 
   try {
     const nhlScoreResponse = await fetch(scoreURL);
     const nhlScore = await nhlScoreResponse.json();
-    console.log(nhlScore);
-    
- if number of objects is 1
-  output 1 object
-else
-  loop through all objects 
-    
-      const hometeamName = nhlScore.gameData.teams.home.abbreviation;
-      const awayteamName = nhlScore.gameData.teams.away.abbreviation;
-      const hometeamScore =
-        nhlScore.liveData.plays.currentPlay.about.goals.home;
-      const awayteamScore =
-        nhlScore.liveData.plays.currentPlay.about.goals.away;
-      const stadium = nhlScore.gameData.venue.name;
-      const period = nhlScore.liveData.plays.currentPlay.period;
-      const clock = nhlScore.liveData.plays.currentPlay.periodTimeRemaining;
-      console.log(hometeamName, awayteamName);
-      nhlContainer.innerHTML += `
-      
+
+    console.log(nhlScore.games);
+    for (let item in nhlScore.games) {
+      console.log(nflscore.games[item].teams);
+    }
+    /* 
+    for (let item in nflScore) {
+      const hometeamName = nflScore[item].home.abbr;
+      const awayteamName = nflScore[item].away.abbr;
+      const hometeamScore = nflScore[item].home.score.T;
+      const awayteamScore = nflScore[item].away.score.T;
+      const stadium = nflScore[item].stadium;
+      const quarter = nflScore[item].qtr;
+      const clock = nflScore[item].clock;
+
+      nflContainer.innerHTML += `
 
       <div class="card">
                 <div class="card-body">
-                <h5 class="card-title"><img class="homelogo" src="./assets/nhl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nhl/${awayteamName}.png" </img> </h5>
+                <h5 class="card-title"><img class="homelogo" src="./assets/nfl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nfl/${awayteamName}.png" </img> </h5>
                 <p class="card-text">Score: ${hometeamScore} - ${awayteamScore}</p>
                 <p class="card-text">Stadium: ${stadium} </p>
-                <p class="card-text">Period: ${period} </p>
+                <p class="card-text">Quarter: ${quarter} </p>
                 <p class="card-text">Clock: ${clock} </p>
             </div>
             </div>
             </div>
       `;
-    
+    } */
   } catch (error) {
     error = ("Nothing here", error);
     console.log(error);
   }
 })();
- */
