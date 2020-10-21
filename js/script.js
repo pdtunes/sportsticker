bea1b5cdef8ae17ee95983d074453b542b892ffa
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-10-21 13:35:55 +0200

    update

--- js/script.js
+++ js/script.js
@@ -49,34 +49,24 @@ import { NHL_SCORE } from "./data/data.js";
     const nhlScoreResponse = await fetch(scoreURL);
     const nhlScore = await nhlScoreResponse.json();
 
-    console.log(nhlScore.games);
-    for (let item in nhlScore) {
-      console.log(nhlScore.games[item].goals);
-    }
-    /* 
-    for (let item in nflScore) {
-      const hometeamName = nflScore[item].home.abbr;
-      const awayteamName = nflScore[item].away.abbr;
-      const hometeamScore = nflScore[item].home.score.T;
-      const awayteamScore = nflScore[item].away.score.T;
-      const stadium = nflScore[item].stadium;
-      const quarter = nflScore[item].qtr;
-      const clock = nflScore[item].clock;
 
-      nflContainer.innerHTML += `
+    nhlScore.games.forEach(match => {
+  
+      const hometeamName = match.teams.home.abbreviation;
+      const awayteamName = match.teams.away.abbreviation;
+ 
+
+      nhlContainer.innerHTML += `
 
       <div class="card">
                 <div class="card-body">
-                <h5 class="card-title"><img class="homelogo" src="./assets/nfl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nfl/${awayteamName}.png" </img> </h5>
-                <p class="card-text">Score: ${hometeamScore} - ${awayteamScore}</p>
-                <p class="card-text">Stadium: ${stadium} </p>
-                <p class="card-text">Quarter: ${quarter} </p>
-                <p class="card-text">Clock: ${clock} </p>
+                <h5 class="card-title"><img class="homelogo" src="./assets/nhl/${hometeamName}.png"</img>${hometeamName} vs ${awayteamName} <img class="awaylogo" src="./assets/nhl/${awayteamName}.png" </img> </h5>
+                <p class="card-text">Score: ${match.scores}</p>
             </div>
             </div>
             </div>
       `;
-    } */
+    })
   } catch (error) {
     error = ("Nothing here", error);
     console.log(error);
